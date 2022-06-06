969b482404231bd18ddc14a9855a341cfec94303
jqjhl <96766882+jqjhl@users.noreply.github.com>
2022-06-06 11:27:28 +0800

    Update fdisk.sh

--- fdisk.sh
+++ fdisk.sh
@@ -11,15 +11,15 @@ EOF
 #创建物理卷
 pvcreate /dev/sda1
 #创建卷组
-vgcreate tmpstore /dev/sda1
+vgcreate datastore /dev/sda1
 #创建逻辑卷
-lvcreate -L 600M -n sw tmpstore
+lvcreate -L 600M -n sw datastore
 #格式化
-mkfs -t xfs /dev/tmpstore/sw
+mkfs -t xfs /dev/tmpstore/database
 #创建挂载点
-mkdir /mnt/tmpbase
+mkdir /mnt/database
 #挂载
-mount /dev/tmpstore/sw /mnt/tmpbase
+mount /dev/datastore/database /mnt/database
 #开机挂载
-echo "/dev/tmpstore/sw /mnt/tmpbase xfs defaults 0 0" >>/etc/fstab
+echo "/dev/datastore/database /mnt/database xfs defaults 0 0" >>/etc/fstab
 
